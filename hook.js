ce1779216ce2307d226b7c3ae71e30efad633cc5
Ritchie Martori <skawful@gmail.com>
2012-11-12 20:18:02 -0800

    added ip whitelist to hook

--- hook.js
+++ hook.js
@@ -4,14 +4,18 @@
 
 require('shelljs/global');
 
-function restart() {
-	
-}
+var allowed = ['207.97.227.253', '50.57.128.197', '108.171.174.178'];
 
 var http = require('http');
 
 http.createServer(function (req, res) {
   var output = '';
+  var ip = req.socket.remoteAddress;
+  
+  if(allowed.indexOf(ip) === -1) {
+    console.error('unkown ip trying hook! ' + ip);
+    return;
+  }
   
   if(req.url === '/hook') {
     output += exec('git pull origin master').output;
