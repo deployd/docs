8bce595763515384ca333afdeaf95cfae3cfe2cb
Ritchie Martori <skawful@gmail.com>
2012-12-06 10:53:24 -0800

    changed default port for nginx

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express')
 app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.logger('dev'));
