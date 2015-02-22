a6cfde867950d2e0c52fe37e777898628656f0a7
Eric Fong <ericff@gmail.com>
2015-02-23 00:57:38 +0800

    Auto-generated deployd docs

--- javascripts/module-control.js
+++ javascripts/module-control.js
@@ -1,23 +1,22 @@
 angular.module('docs', [])
 
 .controller('ModulesCtrl', function($scope, $http) {
+
   $scope.input = {};
-  
-  var allMoudles = [];
-  $.getJSON('http://npm.dathub.org/api/rows?start=dpd-', function(res){
-    $scope.modules = allMoudles = _.filter(res.rows, function(mod){
-      return mod.time.modified >= '2013-01-01';
-    });
-    $scope.$apply();
-  })
+
+  var allModules = $.map(window.Modules, function(value, index) {
+    return value;
+  });
+
+  $scope.modules = allModules;
 
 
   $scope.search = function(){
     var searchFor = $scope.input.searchFor;
     var regex = new RegExp(searchFor);
     $scope.modules = [];
-    for (var i = 0, ii = allMoudles.length; i < ii; i++) {
-      var mod = allMoudles[i];
+    for (var i = 0, ii = allModules.length; i < ii; i++) {
+      var mod = allModules[i];
       if (regex.test(mod.name) || regex.test(mod.description)) {
         $scope.modules.push(mod);
       }
